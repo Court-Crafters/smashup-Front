db7853b4aa7accf9a39520b0a1fa13fd475e1a10
AzizSYW <med.aziz@saveyourwardrobe.com>
2025-01-28 23:19:47 +0100

    adding react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import MainLayout from "./layout";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./assets/css/all.min.css";
 import "./assets/css/animate.css";
 import "./assets/css/magnific-popup.css";
 import "./assets/css/meanmenu.css";
 import "./assets/css/nice-select.css";
 import "./assets/css/main.css";
-import 'swiper/css';
+import "swiper/css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
-    <>
-      <MainLayout />
-    </>
+    <BrowserRouter basename="/">
+      <Routes>
+        <Route path="/" element={<MainLayout />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
