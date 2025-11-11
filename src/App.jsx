655490b6e4cc94ac3f3bf463b8a1f1560477bc1c
Dhaker12 <48828827+Dhaker12@users.noreply.github.com>
2025-11-11 21:19:24 +0100

    privacy policy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import MainLayout from "./layout";
-import ComingSoonPage from "./components/ComingSoonPage";
+import ComingSoonPage from "./components/comingSoonPage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./assets/css/all.min.css";
@@ -9,16 +9,34 @@ import "./assets/css/meanmenu.css";
 import "./assets/css/nice-select.css";
 import "./assets/css/main.css";
 import "swiper/css";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <HashRouter>
       <Routes>
-        {/* Base URL renders MainLayout */}
+        {/* Redirect empty path to home */}
         <Route path="/" element={<MainLayout />} />
+        <Route path="" element={<Navigate to="/" replace />} />
 
-        {/* Any other path redirects to ComingSoonPage */}
+        {/* Privacy Policy page */}
+        <Route
+          path="/privacy-policy"
+          element={
+            <iframe
+              src="/privacy-policy.html"
+              title="Privacy Policy"
+              style={{
+                width: "100%",
+                height: "100vh",
+                border: "none",
+                background: "#fafafa",
+              }}
+            />
+          }
+        />
+
+        {/* Fallback */}
         <Route path="*" element={<ComingSoonPage />} />
       </Routes>
     </HashRouter>
